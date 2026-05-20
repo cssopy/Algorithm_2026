d9bd7e55a46d8374632dc954533fbb21e1d83c56
cssopy <83584721+cssopy@users.noreply.github.com>
2026-05-20 19:58:57 +0900

    Create lv0_PCCE_기출문제_2번_피타고라스의_정리.java

---
+++ PROGRAMMERS/lv0/java/lv0_PCCE_기출문제_2번_피타고라스의_정리.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class lv0_PCCE_기출문제_2번_피타고라스의_정리 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c * c - a * a;
+
+        System.out.println(b_square);
+    }
+}
