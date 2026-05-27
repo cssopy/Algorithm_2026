758b585dd537117261b7395709fdd161c6ba5b22
cssopy <83584721+cssopy@users.noreply.github.com>
2026-05-27 18:29:33 +0900

    Create lv0_문자열_붙여서_출력하기.java

---
+++ PROGRAMMERS/lv0/java/lv0_문자열_붙여서_출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class lv0_문자열_붙여서_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        System.out.println(str1 + str2);
+    }
+}
